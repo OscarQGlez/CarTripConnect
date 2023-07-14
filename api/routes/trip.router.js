a712aba8d4e95ed7b637d2c8e8fcee4ca691e056
OscarQGlez <osquevgon@gmail.com>
2023-07-14 17:22:34 +0100

    general: done

--- api/routes/trip.router.js
+++ api/routes/trip.router.js
@@ -14,13 +14,17 @@ const {                                                                         
     getAllTripsEager } = require('../controllers/trip.controller')
 
 router.get('/', getAllTrips)
+
 router.get('/eager', getAllTripsEager)
+
 router.get('/search',searchAvailableTrips)
 router.get('/searchAllTripsRatings', searchAllTripsRatings)
 router.get('/:id', getOneTrip)
 
 router.post('/', createTrip)
+
 router.post('/offerTrip',checkAuth, offerTrip)
+
 router.post('/provideFeedback', checkAuth, provideFeedback)
 router.post('/addUserTrip/:tripId', checkAuth, addUserTrip)
 
